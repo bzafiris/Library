04b630eff5c8c5b4702c1fc027494324071e7a7c
Vassilis Zafeiris <bzafiris@gmail.com>
2020-02-19 12:07:17 +0200

    Updated borrow logic, added system.out logging

--- src/com/mgiandia/library/service/LoanService.java
+++ src/com/mgiandia/library/service/LoanService.java
@@ -46,7 +46,12 @@ public class LoanService {
         if (borrower == null) {
             throw new LibraryException();
         }
+        if (itemNo == -1) {
+            System.out.println("Borrowing not allowed");
+            return null;
+        }
         if (!borrower.canBorrow()) {
+            System.out.println("Borrowing not allowed");
             return null;
         }
         ItemDAO itemDao = new ItemDAOMemory();
